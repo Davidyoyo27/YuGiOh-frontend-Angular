dd75a372177de0da250811b52098a427fd5d639e
Davidyoyo27 <davidca.vega@gmail.com>
2026-08-29 23:01:02 -0400

    feat[] HU-menu-options-duel : -se completo menu de opciones para los duelos

---
+++ src/app/features/duel/pages/menu-options-duel-page/menu-options-duel-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-menu-options-duel-page',
+  imports: [RouterLink],
+  templateUrl: './menu-options-duel-page.html',
+  styleUrl: './menu-options-duel-page.css',
+})
+export default class MenuOptionsDuelPage {}
